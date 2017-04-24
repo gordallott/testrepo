6848beebb37dcaab1ad3aed6ef4702bcbadb2898
Gord Allott <gordallott@gmail.com>
2017-04-24 22:43:00 +0100

    best at test repos, so good, i have the best test repos

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash 
 
 echo "this is a build.sh, build me"
+sleep 2
+echo "i waited two seconds before this line"
+echo "here is another line"
+echo "okay waiting another two seconds"
+sleep 2
 
+echo " [0;1;35;95m⢇[0;1;31;91m⢸[0m [0;1;33;93m⣏⡉[0m [0;1;32;92m⣎[0;1;36;96m⣱[0m [0;1;34;94m⣎⣱[0m [0;1;35;95m⣎[0;1;31;91m⣱[0m [0;1;33;93m⣇⣸[0m
+  [0;1;33;93m⠇[0m [0;1;32;92m⠧⠤[0m [0;1;36;96m⠇[0;1;34;94m⠸[0m [0;1;35;95m⠇⠸[0m [0;1;31;91m⠇[0;1;33;93m⠸[0m [0;1;32;92m⠇⠸[0m
+  "
